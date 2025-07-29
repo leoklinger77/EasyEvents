8347bf51f8e64385cd98bbe000965c0063df53e4
klinger <klinger@klingersystem.com.br>
2025-07-29 09:13:27 -0300

    nomenclature of operations

---
+++ src/EasyEvent/Queue/EventQueue.cs
@@ -0,0 +1,20 @@
+﻿namespace EasyEvent.Queue {
+    using EasyEvent.Events;
+    using System.Threading.Channels;
+    public class EventQueue {
+        private readonly Channel<IQueue> _channel;
+        public EventQueue(UnboundedChannelOptions? unboundedOptions = null) {
+            _channel = Channel.CreateUnbounded<IQueue>(unboundedOptions ?? new UnboundedChannelOptions());
+        }
+
+        public EventQueue(BoundedChannelOptions boundedOptions) {
+            _channel = Channel.CreateBounded<IQueue>(boundedOptions ?? throw new ArgumentNullException(nameof(boundedOptions)));
+        }
+
+        public async Task EnqueueAsync(IQueue @event) {
+            await _channel.Writer.WriteAsync(@event);
+        }
+
+        public ChannelReader<IQueue> Reader => _channel.Reader;
+    }
+}
