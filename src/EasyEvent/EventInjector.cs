8f2d6c2a200d6e1caae460590f63fada5d85e532
klinger <klinger@klingersystem.com.br>
2025-08-06 21:32:37 -0300

    save progrss

--- src/EasyEvent/EventInjector.cs
+++ src/EasyEvent/EventInjector.cs
@@ -1,10 +1,10 @@
 ﻿namespace EasyEvent {
-	using EasyEvent.Handler;
+    using EasyEvent.Handler;
     using EasyEvent.Options;
     using EasyEvent.Queue;
-	using Microsoft.Extensions.DependencyInjection;
-	public static class EventInjector {
-		public static void AddEasyEvents(this IServiceCollection services, Action<EasyOptions>? configure = null) {
+    using Microsoft.Extensions.DependencyInjection;
+    public static class EventInjector {
+        public static void AddEasyEvents(this IServiceCollection services, Action<EasyOptions>? configure = null) {
             var options = new EasyOptions();
             configure?.Invoke(options);
 
@@ -15,21 +15,19 @@
                 throw new InvalidOperationException("Only one of 'QueueBounded' or 'QueueUnbounded' should be configured, not both.");
             }
 
-            if (!hasBounded && !hasUnbounded) {
-                throw new InvalidOperationException("You must configure either 'QueueBounded' or 'QueueUnbounded'.");
-            }
-
             EventQueue queue;
             if (options.QueueBounded is not null) {
                 queue = new EventQueue(options.QueueBounded);
-            } else {
+            } else if (options.QueueUnbounded is not null) {
                 queue = new EventQueue(options.QueueUnbounded);
+            } else {
+                queue = new EventQueue();
             }
             services.AddSingleton(queue);
 
-			services.AddHostedService<EventDispatcherWorker>();
+            services.AddHostedService<EventDispatcherWorker>();
 
-			services.AddScoped<IEasyEvents, EazyEvents>();
-		}
-	}
+            services.AddScoped<IEasyEvents, EazyEvents>();
+        }
+    }
 }
